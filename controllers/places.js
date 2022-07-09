6f5f0c8b383d3484f18752390f1b14b0a436d1e5
bouzianes <fcbdreams1991@gmail.com>
2022-07-09 10:54:38 -0400

    adding and rendering

--- controllers/places.js
+++ controllers/places.js
@@ -2,6 +2,10 @@ const router = require('express').Router()
 
 
 // GET /places
+router.get('/new', (req, res) => {
+    res.render('places/new')
+  })
+  
 router.get('/', (req, res) => {
     let places = [{
         name: 'TAI',
@@ -19,6 +23,6 @@ router.get('/', (req, res) => {
       
     res.render('places/index', {places})
   })
-  
+ 
 
 module.exports = router
---
+++ views/places/new.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const Def = require('../default')
+function new_form(){
+    return(
+        <Def>
+            <main>
+                <h1>
+                    add new places
+                </h1>
+            </main>
+        </Def>
+    )
+}
+module.exports = new_form
