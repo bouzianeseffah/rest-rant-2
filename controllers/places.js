b5e69b9bbc74618a3d7537c13933619762a12ccd
bouzianes <fcbdreams1991@gmail.com>
2022-07-12 08:30:38 -0400

    delete functionality implented

--- controllers/places.js
+++ controllers/places.js
@@ -53,6 +53,17 @@ router.get('/', (req, res) => {
     res.render()
   })
   router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else{
+        places.splice(id, 1)
+        res.redirect('/places')
+    }
     res.render()
   })
   router.post('/:id/rant', (req, res) => {
