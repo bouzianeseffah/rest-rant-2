604ad8a8ed6f1c1a7f8fb6782d32fde0ab299e3a
bouzianes <fcbdreams1991@gmail.com>
2022-07-27 00:28:53 -0400

    Implement PUT and DELETE

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -5,7 +5,7 @@ function edit_form(data) {
         <Def>
             <main>
                 <h1>Edit place</h1>
-                <form  action={`/places/${data.id}?_method=PUT`} method='POST'>
+                <form  action={`/places/${data.place.id}?_method=PUT`} method='POST'>
                  <div className='row'>
                     <div className='form-group col-sm-6'>
                         <label htmlFor='name'>Place Name</label>
@@ -27,7 +27,15 @@ function edit_form(data) {
                         <label htmlFor='cuisine'>Cuisine</label>
                         <input className='form-control' id='cuisine' name='cuisine' defaultValue={data.place.cuisines}  required/>
                     </div>
-                    <input className='btn btn-primary' type="submit" value='Edit Place' />
+                    <div className='form-group col-sm-4'>
+                        <label htmlFor="founded">Founded</label>
+                        <input className='form-control' 
+                        id='founded'
+                        name='founded'
+                        value={data.place.founded}
+                         />
+                    </div>
+                    <input className='btn btn-primary' type="submit" value='Update Place' />
                  </div> 
                 </form>
             </main>
